21c0254fb7fd536ec02546c93248a3a6ba6ebf8a
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-04-28 21:09:10 +0530

    Day 3: Axios API service (detect, history)

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+})
+
+export const detectDisease = async (file: File) => {
+  const form = new FormData()
+  form.append('file', file)
+  const { data } = await api.post('/detect', form)
+  return data
+}
+
+export const getDetections = async () => {
+  const { data } = await api.get('/detections')
+  return data
+}
+
+export const getDetection = async (id: number) => {
+  const { data } = await api.get(`/detections/${id}`)
+  return data
+}
+
+export const deleteDetection = async (id: number) => {
+  const { data } = await api.delete(`/detections/${id}`)
+  return data
+}
